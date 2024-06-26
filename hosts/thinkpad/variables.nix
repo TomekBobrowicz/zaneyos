4a927f774c63b56688cc700ec0484dce0d3808bb
installer <installer@gmail.com>
2024-06-26 23:16:43 +0200

    initial

---
+++ hosts/thinkpad/variables.nix
@@ -0,0 +1,15 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "Tomek Bobrowicz";
+  gitEmail = "bobrowicz.tomek@gmail.com";
+
+  # Hyprland Settings
+  extraMonitorSettings = "";
+
+  # Waybar Settings
+  clock24h = false;
+
+  # Program Options
+  browser = "google-chrome-stable"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+}
